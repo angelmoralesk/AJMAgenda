7ad0970587d15736fafec4de738fc1a4fd77ba6a
Angel Morales Karam <a.jesse3001@gmail.com>
2017-08-24 13:05:48 -0500

    setting the EventCell through IB instead of registering the cell

--- AJMAgenda/ViewController.swift
+++ AJMAgenda/ViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //collectionView.register(EventCell.self, forCellWithReuseIdentifier: "EventCell")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,5 +24,33 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController : UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+       return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "EventCell", for: indexPath) as! EventCell
+        cell.title = "Evento"
+        cell.backgroundColor = UIColor.red
+        return cell
+    }
+    
+    
+}
+
+extension ViewController : UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 300, height: 300)
+    }
+    
 }
 
